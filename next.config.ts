f788beac969837d448c5f120131b89a9fd5f38f1
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-10 23:51:03 +0800

    fix: export to deploy in github pages

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
-    domains: ['github.com']
-  }
+    domains: ['github.com'],
+  },
 };
 
 export default nextConfig;
