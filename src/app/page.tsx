bda9e2cccbabd9d76d607728e1c7814cf7755b54
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-10 23:42:11 +0800

    initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,233 @@
 import Image from "next/image";
+import { Home, Edit, Globe, Info } from "lucide-react";
 
-export default function Home() {
+import { MdEmail } from "react-icons/md";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebookF, FaLaptop } from "react-icons/fa";
+import { HiMiniCodeBracket } from "react-icons/hi2";
+import { IoLogoJavascript, IoMdPhonePortrait } from "react-icons/io";
+import { FiDatabase } from "react-icons/fi";
+import { FaComputer } from "react-icons/fa6";
+import { BsEarbuds } from "react-icons/bs";
+
+export default function HomePage() {
+  const navLinks = [
+    {
+      icon: Home,
+      title: "Home",
+    },
+    {
+      icon: Edit,
+      title: "Edit",
+    },
+    {
+      icon: Globe,
+      title: "Friends",
+    },
+    {
+      icon: Info,
+      title: "Info",
+    },
+  ];
+
+  const contacts = [
+    {
+      icon: MdEmail,
+    },
+    {
+      icon: AiFillInstagram,
+    },
+    {
+      icon: FaFacebookF,
+    },
+  ];
+
+  const skills = [
+    {
+      icon: HiMiniCodeBracket,
+      title: "HTML & CSS",
+    },
+    {
+      icon: IoLogoJavascript,
+      title: "JavaScript",
+    },
+    {
+      icon: FiDatabase,
+      title: "Database",
+    },
+  ];
+
+  const equipments = [
+    {
+      icon: FaComputer,
+      title: "Main Desktop",
+    },
+    {
+      icon: FaLaptop,
+      title: "ROG Strix G17",
+    },
+    {
+      icon: IoMdPhonePortrait,
+      title: "Samsung A54",
+    },
+    {
+      icon: BsEarbuds,
+      title: "Galaxy Buds 2",
+    },
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col w-full sm:max-w-7xl mx-auto">
+      <header className="flex flex-col justify-center items-center gap-4 p-8">
+        <div className="relative size-32 rounded-full overflow-clip">
+          <Image
+            src={"/profile-pic.jpg"}
+            fill
+            className="w-full h-full object-cover object-center"
+            alt="Reece Lim"
+          ></Image>
+        </div>
+        <h1 className="text-3xl font-bold">Loading</h1>
+        <p>A clever person solves a problem. A wise person avoids it.</p>
+      </header>
+      <main className="flex flex-col gap-2">
+        <nav className="bg-neutral-900 h-12 flex justify-center items-center">
+          {navLinks.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className="flex items-center gap-1 p-2 px-3 hover:bg-neutral-600 transition-all duration-300 ease-in-out h-full"
+              >
+                <item.icon className="size-5" />
+                {item.title}
+              </button>
+            );
+          })}
+        </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className="flex gap-2">
+          <div className="bg-neutral-900 flex-grow-[1] p-6 flex flex-col items-center gap-4">
+            <div className="relative overflow-clip size-24 rounded-full">
+              <Image
+                src={"https://github.com/NotRelix.png"}
+                fill
+                className="w-full h-full object-cover object-center"
+                alt="profile picture"
+              ></Image>
+            </div>
+            <h1 className="text-3xl font-bold">Reece Lim</h1>
+            <p className="font-bold">hi there</p>
+            <div className="flex gap-2">
+              {contacts.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="border-2 rounded-full hover:bg-neutral-500 transition-all ease-in-out"
+                  >
+                    <item.icon className="size-10 p-2"></item.icon>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex-grow-[9] bg-neutral-500 relative overflow-clip">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              src={"/hero-banner.jpg"}
+              fill
+              className="w-full h-full object-cover object-top"
+              alt="background scenery image"
+            ></Image>
+          </div>
+        </div>
+
+        <div className="bg-neutral-900 p-8 flex flex-col gap-4">
+          <h1 className="border-l-[6px] border-blue-400 px-2 font-bold text-3xl">
+            Skills
+          </h1>
+          <div className="flex gap-2">
+            {skills.map((item) => {
+              return (
+                <div className="bg-neutral-800/50 p-4 flex flex-col gap-4 flex-1 hover:bg-neutral-700 transition-all ease-in-out">
+                  <item.icon className="size-12" />
+                  <p className="font-bold">{item.title}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="bg-neutral-900 p-8 flex flex-col gap-4">
+          <h1 className="border-l-[6px] border-blue-400 px-2 font-bold text-3xl">
+            Equipments
+          </h1>
+          <div className="flex gap-2">
+            {equipments.map((item) => {
+              return (
+                <div className="bg-neutral-800/50 p-4 flex flex-col gap-4 flex-1 hover:bg-neutral-700 transition-all ease-in-out">
+                  <item.icon className="size-12" />
+                  <p className="font-bold">{item.title}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="bg-neutral-900 p-8 flex flex-col gap-4">
+          <h1 className="border-l-[6px] border-blue-400 px-2 font-bold text-3xl">
+            Cool Pictures
+          </h1>
+          <div className="grid-container h-[36rem] w-full gap-2">
+            <div className="main-picture w-full h-full relative overflow-clip">
+              <Image
+                src="/pictures/kyoto-japan.jpeg"
+                fill
+                alt="a cozy pavillion in japan"
+                className="object-cover"
+              />
+            </div>
+            <div className="remaining-pictures">
+              <div className="main-picture w-full h-full relative overflow-clip">
+                <Image
+                  src="/pictures/shanghai-china.jpeg"
+                  fill
+                  alt="shanghai, china's city proper scenery"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+            <div className="remaining-pictures">
+              <div className="main-picture w-full h-full relative overflow-clip">
+                <Image
+                  src="/pictures/el-nido-philippines.jpeg"
+                  fill
+                  alt="a beach in el nido in the philippines"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+            <div className="remaining-pictures">
+              <div className="main-picture w-full h-full relative overflow-clip">
+                <Image
+                  src="/pictures/new-york-usa.jpeg"
+                  fill
+                  alt="city proper in new york, usa with tall buildings"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+            <div className="remaining-pictures">
+              <div className="main-picture w-full h-full relative overflow-clip">
+                <Image
+                  src="/pictures/london-uk.jpeg"
+                  fill
+                  alt="driving in london in a rainy day"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
