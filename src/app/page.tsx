679fa2289987b67fb03ddd38a2ccd8218feb0122
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-02-11 14:04:03 +0800

    feat: image overlay on cool pictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { FaFacebookF, FaLaptop } from "react-icons/fa";
 import { HiMiniCodeBracket } from "react-icons/hi2";
 import { IoLogoJavascript, IoMdPhonePortrait } from "react-icons/io";
 import { FiDatabase } from "react-icons/fi";
-import { FaComputer } from "react-icons/fa6";
+import { FaComputer, FaLocationDot } from "react-icons/fa6";
 import { BsEarbuds } from "react-icons/bs";
 
 export default function HomePage() {
@@ -145,9 +145,9 @@ export default function HomePage() {
             Skills
           </h1>
           <div className="flex gap-2">
-            {skills.map((item) => {
+            {skills.map((item, index) => {
               return (
-                <div className="bg-neutral-800/50 p-4 flex flex-col gap-4 flex-1 hover:bg-neutral-700 transition-all ease-in-out">
+                <div className="bg-neutral-800/50 p-4 flex flex-col gap-4 flex-1 hover:bg-neutral-700 transition-all ease-in-out" key={index}>
                   <item.icon className="size-12" />
                   <p className="font-bold">{item.title}</p>
                 </div>
@@ -176,53 +176,73 @@ export default function HomePage() {
           <h1 className="border-l-[6px] border-blue-400 px-2 font-bold text-3xl">
             Cool Pictures
           </h1>
-          <div className="grid-container h-[36rem] w-full gap-2">
-            <div className="main-picture w-full h-full relative overflow-clip">
+          <div className="grid grid-cols-3 grid-rows-2 h-[36rem] w-full gap-2">
+            <div className="col-span-1 row-span-2 relative w-full h-full overflow-hidden rounded-xl">
               <Image
                 src="/pictures/kyoto-japan.jpeg"
                 fill
                 alt="a cozy pavillion in japan"
                 className="object-cover"
               />
+              <span className="absolute bottom-2 left-2 p-2 items-center rounded-xl bg-neutral-900/50 backdrop-blur-sm flex gap-2">
+                <FaLocationDot />
+                <span>Kyoto, Japan</span>
+              </span>
             </div>
-            <div className="remaining-pictures">
-              <div className="main-picture w-full h-full relative overflow-clip">
+            <div className="col-span-1 row-span-1">
+              <div className="w-full h-full relative overflow-clip rounded-xl">
                 <Image
                   src="/pictures/shanghai-china.jpeg"
                   fill
                   alt="shanghai, china's city proper scenery"
                   className="object-cover"
                 />
+                <span className="absolute bottom-2 left-2 p-2 items-center rounded-xl bg-neutral-900/50 backdrop-blur-sm flex gap-2">
+                  <FaLocationDot />
+                  <span>Shanghai, China</span>
+                </span>
               </div>
             </div>
-            <div className="remaining-pictures">
-              <div className="main-picture w-full h-full relative overflow-clip">
+            <div className="col-span-1 row-span-1">
+              <div className="main-picture w-full h-full relative overflow-clip rounded-xl">
                 <Image
                   src="/pictures/el-nido-philippines.jpeg"
                   fill
                   alt="a beach in el nido in the philippines"
                   className="object-cover"
                 />
+                <span className="absolute bottom-2 left-2 p-2 items-center rounded-xl bg-neutral-900/50 backdrop-blur-sm flex gap-2">
+                  <FaLocationDot />
+                  <span>El Nido, Philippines</span>
+                </span>
               </div>
             </div>
-            <div className="remaining-pictures">
-              <div className="main-picture w-full h-full relative overflow-clip">
+            <div className="col-span-1 row-span-1">
+              <div className="main-picture w-full h-full relative overflow-clip rounded-xl">
                 <Image
                   src="/pictures/new-york-usa.jpeg"
                   fill
                   alt="city proper in new york, usa with tall buildings"
                   className="object-cover"
                 />
+                <span className="absolute bottom-2 left-2 p-2 items-center rounded-xl bg-neutral-900/50 backdrop-blur-sm flex gap-2">
+                  <FaLocationDot />
+                  <span>New York, USA</span>
+                </span>
               </div>
             </div>
-            <div className="remaining-pictures">
-              <div className="main-picture w-full h-full relative overflow-clip">
+            <div className="col-span-1 row-span-1">
+              <div className="main-picture w-full h-full relative overflow-clip rounded-xl">
                 <Image
                   src="/pictures/london-uk.jpeg"
                   fill
                   alt="driving in london in a rainy day"
                   className="object-cover"
                 />
+                <span className="absolute bottom-2 left-2 p-2 items-center rounded-xl bg-neutral-900/50 backdrop-blur-sm flex gap-2">
+                  <FaLocationDot />
+                  <span>London, UK</span>
+                </span>
               </div>
             </div>
           </div>
